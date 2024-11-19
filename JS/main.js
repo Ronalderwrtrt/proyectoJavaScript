2ae9fa5da5e6e0331f609feadf00c63afa9b676b
marianal17 <marianaalarcon2006@cesanrafael.org>
2024-11-19 12:28:28 -0400

    index

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+let mensaje= "hola mundo";
+
+function sumar(){
+let numero1=3;
+let numero2=4;
+let suma=numero1+numero2
+alert("la suma es"+suma)
+}
+
+
+function restar(){
+    let numero1=3;
+    let numero2=4;
+    let restar=numero1-numero2
+    alert("la resta es"+restar)
+    }
+
+    
+function multiplicar(){
+    let numero1=3;
+    let numero2=4;
+    let multiplicar=numero1*numero2
+    alert("la multiplicacion es"+multiplicar)
+    }
+
+    
+function dividir(){
+    let numero1=3;
+    let numero2=4;
+    let dividir=numero1/numero2
+    alert("la divicion es"+dividir)
+    }
